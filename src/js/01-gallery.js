d3e2d74990976d68404966b797767380beb16594
Nastia-Chorna <82803581+Nastia-Chorna@users.noreply.github.com>
2021-10-29 18:59:45 +0300

    fisrt push

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,19 @@
-// Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryImage = document.querySelector(".gallery");
+
+function addGallery(galleryItems) {
+  const markup = galleryItems.map(img =>
+`<a class="gallery__item" href="${img.original}">
+<img class="gallery__image" src="${img.preview}" alt="${img.description}" />
+</a>`).join('');
+  galleryImage.innerHTML = markup
+};
+addGallery(galleryItems);
+
+const gallery = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
