482553c63454e3bf4f04a0f800956bc90a8f50b7
Nastia-Chorna <82803581+Nastia-Chorna@users.noreply.github.com>
2021-10-29 19:26:44 +0000

    Deploying to gh-pages from @ Nastia-Chorna/goit-js-hw-08@52ea5ef2f546f2ac3cefeaca62acd336d5f610d9 🚀

--- 01-gallery.a1ca4a57.js
+++ 01-gallery.a1ca4a57.js
@@ -8,4 +8,4 @@ var t=arguments[3];function e(t){return(e="function"==typeof Symbol&&"symbol"==t
 },{}],"TfQ5":[function(require,module,exports) {
 "use strict";var e=i(require("simplelightbox"));require("simplelightbox/dist/simple-lightbox.min.css");var l=require("./gallery-items");function i(e){return e&&e.__esModule?e:{default:e}}const a=document.querySelector(".gallery");function r(e){const l=e.map(e=>`<a class="gallery__item" href="${e.original}">\n<img class="gallery__image" src="${e.preview}" alt="${e.description}" />\n</a>`).join("");a.innerHTML=l}r(l.galleryItems);const t=new e.default(".gallery a",{captionDelay:250,captionsData:"alt"});
 },{"simplelightbox":"yx6k","simplelightbox/dist/simple-lightbox.min.css":"kz9M","./gallery-items":"q5Rn"}]},{},["TfQ5"], null)
-//# sourceMappingURL=/parcel-project-template/01-gallery.a1ca4a57.js.map
+//# sourceMappingURL=/goit-js-hw-08/01-gallery.a1ca4a57.js.map
